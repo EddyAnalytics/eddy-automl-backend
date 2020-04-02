a1919159390db63b741e1b70b60e9443856132b8
Dré van Oorschot <d.h.m.a.vanoorschot@student.utwente.nl>
2020-04-02 17:27:08 +0200

    Add db to cluster config

--- app/settings.py
+++ app/settings.py
@@ -34,7 +34,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'graphene_django'
+    'graphene_django',
+    'api'
 ]
 
 MIDDLEWARE = [
