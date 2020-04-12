796484fe0328445c99e4c73f432af375a6139d38
Alexandru-Ionut Imbrea <aleximbrea@gmail.com>
2020-04-12 22:46:55 +0200

    Add health check for mysql

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+until mysql --host=$DB_HOST --port=$DB_PORT --user=$DB_USER --password=$DB_PASS --execute="SHOW DATABASES;" $DB_NAME; do
+  sleep 1
+done
+
 if [ "$1" = 'app' ]; then
     python3 manage.py migrate --no-input
     python3 manage.py collectstatic --no-input
