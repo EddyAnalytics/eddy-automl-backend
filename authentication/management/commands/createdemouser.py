969f345b5d911ca4302c391be077093d2be85dda
Alexandru-Ionut Imbrea <aleximbrea@gmail.com>
2020-04-12 22:48:57 +0200

    Use email as username for the demo user

--- authentication/management/commands/createdemouser.py
+++ authentication/management/commands/createdemouser.py
@@ -8,8 +8,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         demo_user = authentication.models.User()
-        demo_user.username = 'demo'
-        demo_user.email = 'demo@demo.com'
+        demo_user.username = 'demo@demo.com'
         demo_user.set_password('demo')
         demo_user.save()
         self.stdout.write('Demo user created')
