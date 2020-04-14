ff86dda229a7a12cbaad4b989110418a40a130b5
Dré van Oorschot <d.h.m.a.vanoorschot@student.utwente.nl>
2020-04-14 19:04:18 +0200

    Add name and status field to job model

--- api/models.py
+++ api/models.py
@@ -1,9 +1,20 @@
+from enum import Enum
 
 from django.db import models
 
-
 # Create your models here.
 from authentication.models import User
+
+
+class JobStatus(Enum):
+    RUNNING = 0
+    STOPPED = 1
+
+    FAILED = -1
+
+    @classmethod
+    def choices(cls):
+        return tuple((i.name, i.value) for i in cls)
 
 
 class AutoMLJob(models.Model):
@@ -12,6 +23,10 @@ class AutoMLJob(models.Model):
     output_topic = models.CharField(max_length=100)
     target_column = models.CharField(max_length=100)
 
+    name = models.CharField(max_length=100)
+
+    status = models.IntegerField(choices=JobStatus.choices())
+
     @classmethod
     def create(cls, user, input_topic, output_topic, target_column):
         job = cls(user=user,
@@ -19,3 +34,5 @@ class AutoMLJob(models.Model):
                   output_topic=output_topic,
                   target_column=target_column)
         return job
+
+
