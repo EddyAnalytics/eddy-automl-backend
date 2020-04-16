b058926ed347b6111722d5c37886281d5753d8f9
Alexandru-Ionut Imbrea <aleximbrea@gmail.com>
2020-04-16 13:08:49 +0200

    Fix demo user creation command

--- authentication/management/commands/createdemouser.py
+++ authentication/management/commands/createdemouser.py
@@ -7,8 +7,11 @@ class Command(BaseCommand):
     help = 'Creates default demo user'
 
     def handle(self, *args, **options):
-        demo_user = authentication.models.User()
-        demo_user.username = 'demo@demo.com'
-        demo_user.set_password('demo')
-        demo_user.save()
-        self.stdout.write('Demo user created')
+        try:
+            obj = authentication.models.User.objects.get(username = 'demo@demo.com')
+        except authentication.models.User.DoesNotExist:
+                demo_user = authentication.models.User()
+                demo_user.username = 'demo@demo.com'
+                demo_user.set_password('demo')
+                demo_user.save()
+                self.stdout.write('Demo user created')
